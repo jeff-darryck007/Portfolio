7ca6d03c7243178ab2cfb43b8322ad3a83c0e2a5
ktbm <boliventtalla12@gmail.com>
2026-03-15 02:49:03 +0100

    update

--- core/urls.py
+++ core/urls.py
@@ -53,5 +53,6 @@ urlpatterns = [
     path('jeff-admin-login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
     
 ]
